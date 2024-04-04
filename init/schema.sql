417c20241f231c66d7c26b0579a54fabbc5b3075
Eduardo da Silva Sichelero <135671476+eduardosichelero@users.noreply.github.com>
2024-04-04 11:55:11 -0300

    Implementa novas funcionalidades e adiciona testes

--- init/schema.sql
+++ init/schema.sql
@@ -12,17 +12,16 @@ CREATE TABLE "attendees" (
     "email" TEXT NOT NULL,
     "event_id" TEXT NOT NULL,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "attendees_event_id_fkey" FOREIGN KEY ("event_id")
-    REFERENCES "events" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT "attendees_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE "check_ins" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "attendee_id" TEXT NOT NULL,
-    FOREIGN KEY ("attendee_id") REFERENCES "attendees" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    "attendeeId" TEXT NOT NULL,
+    CONSTRAINT "check_ins_attendeeId_fkey" FOREIGN KEY ("attendeeId") REFERENCES "attendees" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
-CREATE UNIQUE INDEX "events_slug_key" ON "events" ("slug");
-CREATE UNIQUE INDEX "attendees_event_id_email_key" ON "attendees" ("event_id", "email");
-CREATE UNIQUE INDEX "check_ins_attendee_id_key" ON "check_ins" ("attendee_id");
+CREATE UNIQUE INDEX "events_slug_key" ON "events"("slug");
+CREATE UNIQUE INDEX "attendees_event_id_email_key" ON "attendees"("event_id", "email");
+CREATE UNIQUE INDEX "check_ins_attendeeId_key" ON "check_ins"("attendeeId");
--- src/models/settings/base.py
+++ src/models/settings/base.py
@@ -1,4 +1,3 @@
-#constroi uma base declarativa, mostrando pro slq que as tabelas existem
 from sqlalchemy.ext.declarative import declarative_base
 
-base = declarative_base()
+Base = declarative_base()
